9e9d41507df10704474cdc66683e66dacbd4d286
yasirlabs <mubermij@gmail.com>
2026-05-09 21:07:41 +0300

    fix some issues to deploy on Netlify

--- src/app/@modal/(.)articles/[slug]/page.tsx
+++ src/app/@modal/(.)articles/[slug]/page.tsx
@@ -2,8 +2,9 @@ import { getArticleBySlug } from "@/lib/articles";
 import { notFound } from "next/navigation";
 import ArticleModal from "@/components/articles/ArticleModal";
 
-export default function ArticleModalPage({ params }: { params: { slug: string } }) {
-  const article = getArticleBySlug(params.slug);
+export default async function ArticleModalPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+  const article = getArticleBySlug(slug);
   if (!article) notFound();
   return <ArticleModal article={article} />;
 }
