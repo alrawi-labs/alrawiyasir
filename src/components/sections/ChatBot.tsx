ccebb2f4b8cfab851993498e2f3deb20020bdcb1
yasirlabs <mubermij@gmail.com>
2026-05-09 20:50:36 +0300

    change currect url

--- src/components/sections/ChatBot.tsx
+++ src/components/sections/ChatBot.tsx
@@ -130,7 +130,7 @@ const ChatBot = ({ isOpen, setIsOpen }: ChatBotProps) => {
     setLoading(true);
 
     try {
-      const res = await fetch("http://127.0.0.1:7860/ask", {
+      const res = await fetch("https://yasir723-Rag_Based_Portfolio.hf.space/ask", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
