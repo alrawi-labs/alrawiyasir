a1f9c0820fba861e70cdc0a69b30769d1bc66c40
yasirlabs <mubermij@gmail.com>
2026-05-24 16:12:30 +0300

    update chatbot

--- src/components/sections/ChatBot.tsx
+++ src/components/sections/ChatBot.tsx
@@ -525,7 +525,7 @@ const ChatBot = ({ isOpen, setIsOpen }: ChatBotProps) => {
     setLoading(true);
 
     try {
-      const res = await fetch("http://127.0.0.1:7860/ask", {
+      const res = await fetch("https://yasir723-rag-based-portfolio.hf.space/ask", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
